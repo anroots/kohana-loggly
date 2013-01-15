ba91308045acb6b421cf3b22fedc8839184d0b84
Ando Roots <ando@sqroot.eu>
2013-01-15 12:10:51 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# Kohana 3.3 log writer for Loggly
+
+Write Kohana logs to [Loggly](http://loggly.com). Log messages are sent to Loggly via external HTTP requests using the
+`Request::factory()`.
+
+## Installation
+
+### Place the files in your modules directory.
+
+#### As a Git submodule:
+
+```bash
+git clone git://github.com/anroots/kohana-loggly.git modules/loggly
+```
+#### As a [Composer dependency](http://getcomposer.org)
+
+```javascript
+{
+	"require": {
+		"php": ">=5.4.0",
+		"composer/installers": "*",
+		"anroots/kohana-loggly":"1.*"
+	}
+}
+```
+
+### Activate the module in `bootstrap.php`.
+
+```php
+<?php
+Kohana::modules(array(
+	...
+	'loggly' => MODPATH.'loggly',
+));
+```
+
+### Add the log writer after module activation
+
+```php
+<?php
+Kohana::$log->attach(new Log_Loggly('my-input-key'));
+```
+
+You can use the  `$levels` and `$min_level` params of `$log->attach` to set restraints on when to log to Loggly.
+
+### Log some data in your code
+
+```php
+<?php
+Kohana::$log->add(Log::EMERGENCY,'The world will end on :time.',[':time'=>time()+60]);
+```
+
+# Licence
+
+Licenced under the MIT licence.
---
+++ classes/Kohana/Log/Loggly.php
@@ -0,0 +1,70 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+/**
+ * Loggly log writer.
+ *
+ * Writes application logs to the Loggly.com SAAS over an external HTTPS request
+ *
+ * @package    Kohana/Loggly
+ * @category   Logging
+ * @author     Ando Roots <ando@sqroot.eu>
+ * @copyright  (c) 2013 Ando Roots
+ */
+class Kohana_Log_Loggly extends Log_Writer
+{
+
+	const LOGGLY_KEY_LENGTH = 36;
+	const LOGGLY_INPUT_URL = 'https://logs.loggly.com/inputs/';
+
+	/**
+	 * Log request timeout in seconds.
+	 * The external logging request will be aborted if it takes longer than this.
+	 */
+	const LOG_REQUEST_TIMEOUT = 6;
+
+	/**
+	 * @var string Loggly input key
+	 */
+	private $_loggly_input_key;
+
+	public function __construct($loggly_input_key)
+	{
+		if (! Valid::exact_length($loggly_input_key, self::LOGGLY_KEY_LENGTH))
+		{
+			throw new Kohana_Exception('Loggly input key must be exactly :length characters long.', [
+				':length' => self::LOGGLY_KEY_LENGTH
+			]);
+		}
+		$this->_loggly_input_key = $loggly_input_key;
+	}
+
+
+	/**
+	 * Write an array of messages.
+	 *
+	 *     $writer->write($messages);
+	 *
+	 * @param   array   $messages
+	 * @return  void
+	 */
+	public function write(array $messages)
+	{
+		foreach ($messages as $message)
+		{
+
+			Request::factory(
+				self::LOGGLY_INPUT_URL.$this->_loggly_input_key,
+				[
+					CURLOPT_TIMEOUT => self::LOG_REQUEST_TIMEOUT
+				]
+			)
+				->headers('Content-Type', 'application/json')
+				->body($this->format_message($message))
+				->execute();
+		}
+	}
+
+	public function format_message(array $message, $format = "time --- level: body in file:line")
+	{
+		return json_encode($message);
+	}
+}
